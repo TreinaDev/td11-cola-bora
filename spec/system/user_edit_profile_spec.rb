8d59bc83e1360955b96c7c2c4881f8d00fd0448e
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-01-17 12:09:48 -0300

    Adiciona teste de edição de perfil

--- spec/system/user_edit_profile_spec.rb
+++ spec/system/user_edit_profile_spec.rb
@@ -18,7 +18,28 @@ describe 'Usuário edita perfil' do
     expect(page).to have_button 'Salvar Perfil'
   end
 
-  xit 'com sucesso'
+  it 'com sucesso' do
+    user = create(:user)
+    create(:profile, user:, first_name: '', second_name: '',
+                     work_experience: '', education: '')
+
+    login_as user, scope: :user
+    visit edit_profile_path user.profile
+    fill_in 'Nome', with: 'Pedro'
+    fill_in 'Sobrenome', with: 'Silva'
+    fill_in 'Experiência profissional', with: 'Programador, Designer'
+    fill_in 'Informação acadêmica', with: 'Ciências da Computação'
+    click_on 'Salvar Perfil'
+
+    expect(current_path).to eq profile_path(user.profile)
+    expect(page).to have_content 'Perfil atualizado com sucesso!'
+    expect(page).to have_content 'Nome: Pedro'
+    expect(page).to have_content 'Sobrenome: Silva'
+    expect(page).to have_content 'Experiência profissional: Programador, Designer'
+    expect(page).to have_content 'Informação acadêmica: Ciências da Computação'
+  end
+
+  xit 'e volta para pagina de perfil'
   xit 'deixa campo vazio'
   xit 'não é o dono do perfil'
   xit 'não está autenticado'
