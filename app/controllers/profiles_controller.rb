0c76e31ef6cdb75bb0cfb16e7ae7409b1e37bd0e
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-01-19 16:36:33 -0300

    Cria método que define perfil no controller
    Co-authored-by: Lucas Aguilar <lucasaguilar835@gmail.com>
    Co-authored-by: Moises Almeida <moisesssprg@gmail.com>

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,23 +1,22 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!, only: %i[show edit update]
   before_action :authorize_user, only: %i[edit update]
+  before_action :set_profile, only: %i[show edit update]
 
-  def show
-    @profile = current_user.profile
-  end
+  def show; end
 
-  def edit
-    @profile = current_user.profile
-  end
+  def edit; end
 
   def update
-    @profile = current_user.profile
-
     @profile.update(profile_params)
     redirect_to profile_path(@profile), notice: t('.success')
   end
 
   private
+
+  def set_profile
+    @profile = current_user.profile
+  end
 
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :work_experience,
