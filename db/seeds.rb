404fa9a4005af559face4fd9acbe98bb5354464a
Lucas Vasques <lucasvc1996@gmail.com>
2024-01-19 15:32:10 -0300

    Adiciona seed de tarefas
    Co-authored-by: Valéria Carneiro
    <valerialribeiroc@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,18 @@ ash = FactoryBot.create(:user, email: 'ash@email.com')
 brock = FactoryBot.create(:user, email: 'brock@email.com', cpf: '000.000.001-91')
 
 FactoryBot.create(:project, user: ash)
-FactoryBot.create(:project, user: brock, title: 'Líder de Ginásio', description: 'Me tornar líder do estádio de pedra.', category: 'Auto Ajuda') 
+pokemon_project = FactoryBot.create(:project, user: brock, title: 'Líder de Ginásio', 
+                                              description: 'Me tornar líder do estádio de pedra.',
+                                              category: 'Auto Ajuda') 
+
+FactoryBot.create(:task, project: pokemon_project, title:'Pegar um geodude', 
+                         description:'Para completar o meu time de pedra, preciso de um geodude, vamos captura-lo',
+                         assigned: brock, due_date: 2.months.from_now.to_date, author: brock)
+
+FactoryBot.create(:task, project: pokemon_project, title:'Parar a equipe rocket', 
+                         description:'A equipe rocket está aprontando novamente, temos que para-los',
+                         assigned: ash, due_date: Time.zone.tomorrow, author: ash)
+
+FactoryBot.create(:task, project: pokemon_project, title:'Derrotar um Charmander',
+                         description: 'Lutar contra outro treinador com um Charmander.',
+                         assigned: ash, due_date: 1.week.from_now, author: ash)
