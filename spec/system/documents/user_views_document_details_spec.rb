9991e23c97dd1c8e7f4f5bb18a132f74650ff87b
Frederico <frederico.smozzato@gmail.com>
2024-01-23 17:39:07 -0300

    Implementa visualização de documento por colaborador que não é dono do documento
    Co-authored-by: André <andreporteira@gmail.com>
    Co-authored-by: Moisés <moisesssprg@gmail.com>

--- spec/system/documents/user_views_document_details_spec.rb
+++ spec/system/documents/user_views_document_details_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-describe 'Usuário vê detalhes de um documento' do
-  it 'do qual é dono' do
-    user = create(:user)
-    project = create(:project, title: 'Projeto teste', user:)
-    document = project.documents.build(title: 'Documento teste', description: 'Descrição teste', user:)
+describe 'Colaborador vê detalhes de um documento' do
+  it 'que ele adicionou ao projeto' do
+    project = create(:project, title: 'Projeto teste')
+    contributor = create(:user, cpf: '391.503.760-55', email: 'user@email.com')
+    project.user_roles.create!(user: contributor)
+    document = project.documents.build(title: 'Documento teste', description: 'Descrição teste', user: contributor)
     document.file.attach(io: File.open('spec/support/files/imagem1.jpg'), filename: 'imagem1.jpg')
     document.save!
 
-    login_as user, scope: :user
+    login_as contributor, scope: :user
     visit project_documents_path(project)
     click_on 'Documento teste'
 
@@ -21,5 +22,24 @@ describe 'Usuário vê detalhes de um documento' do
     expect(page).to have_current_path document_path(document)
   end
 
-  it 'do qual não é dono'
+  it 'que ele não adicionou ao projeto' do
+    project = create(:project, title: 'Projeto Teste')
+    document_creator = create(:user, cpf: '492.294.960-73', email: 'contributor1@email.com')
+    contributor = create(:user, cpf: '958.314.240-90', email: 'contributor2@email.com')
+    project.user_roles.create!([{ user: document_creator }, { user: contributor }])
+    document = project.documents.build(title: 'Documento teste', description: 'Descrição teste', user: document_creator)
+    document.file.attach(io: File.open('spec/support/files/imagem1.jpg'), filename: 'imagem1.jpg')
+    document.save!
+
+    login_as contributor
+    visit document_path document
+
+    expect(page).to have_content 'Documento teste'
+    expect(page).to have_content 'Descrição teste'
+    expect(page).to have_selector 'img[src$="imagem1.jpg"]'
+    expect(page).to have_link 'Download'
+    expect(page).not_to have_button 'Arquivar'
+  end
+
+  it 'de um projeto ao qual não pertence'
 end
