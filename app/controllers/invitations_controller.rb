c392d2ec4f2637d33a102b75fcffb04a559915a0
Valéria Carneiro <valerialribeiroc@gmail.com>
2024-01-25 15:22:51 -0300

    Refatora controller de convites
    Co-authored-by: Paulo Henrique Meneses <ph.1928@hotmail.com>

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,27 +1,31 @@
 class InvitationsController < ApplicationController
-  def create
-    @invitation = Invitation.new(invitation_params)
-    @invitation.project = Project.find(params[:project_id])
+  before_action :set_invitation, only: %i[create]
 
+  def create
     if @invitation.save
-      redirect_to project_portfoliorrr_profile_path(@invitation.project, invitation_params[:profile_id]),
-                  notice: t('.success')
-    else
-      redirect_to project_portfoliorrr_profile_path(@invitation.project, invitation_params[:profile_id]),
-                  notice: t('.pending_invitation')
+      return redirect_to project_portfoliorrr_profile_path(@invitation.project, @invitation.profile_id),
+                         notice: t('.success')
     end
+    redirect_to project_portfoliorrr_profile_path(@invitation.project, @invitation.profile_id),
+                notice: t('.pending_invitation')
   end
 
   def cancel
     invitation = Invitation.find(params[:id])
     invitation.cancelled!
 
-    redirect_to project_portfoliorrr_profile_path(invitation.project, invitation.profile_id), notice: 'Convite cancelado!'
+    redirect_to project_portfoliorrr_profile_path(invitation.project, invitation.profile_id), notice: t('.success')
   end
 
   private
 
   def invitation_params
-    params.require(:invitation).permit(:profile_id, :expiration_days)
+    params.require(:invitation).permit(:expiration_days)
+  end
+
+  def set_invitation
+    @invitation = Invitation.new(invitation_params)
+    @invitation.project = Project.find(params[:project_id])
+    @invitation.profile_id = params[:portfoliorrr_profile_id]
   end
 end
---
+++ spec/requests/invitations/user_change_invitation_status_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe 'Usuário tenta cancelar convite com status diferente de pending' do
+  pending 'sem sucesso'
+end
