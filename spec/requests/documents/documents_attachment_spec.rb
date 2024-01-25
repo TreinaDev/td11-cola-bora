64975a6e249fcb98fffb07b4dcbe430faab099f4
Frederico <frederico.smozzato@gmail.com>
2024-01-25 08:07:41 -0300

    Testa cenários de upload sem sucesso

--- spec/requests/documents/documents_attachment_spec.rb
+++ spec/requests/documents/documents_attachment_spec.rb
@@ -1,198 +1,224 @@
 require 'rails_helper'
 
-RSpec.describe 'Documento', type: :request do
-  describe 'Documento é anexado ao projeto' do
-    context 'com sucesso' do
-      it 'pelo líder de projeto' do
-        leader = create(:user)
-        project = create(:project, user: leader)
-        params = { document: {
-          title: 'Teste de request',
-          description: 'Descrição',
-          file: fixture_file_upload('spec/support/files/sample_jpg.jpg')
-        } }
+describe 'Documento é anexado ao projeto' do
+  context 'com sucesso' do
+    it 'pelo líder de projeto' do
+      leader = create(:user)
+      project = create(:project, user: leader)
+      params = { document: {
+        title: 'Teste de request',
+        description: 'Descrição',
+        file: fixture_file_upload('spec/support/files/sample_jpg.jpg')
+      } }
 
-        login_as leader, scope: :user
-        post(project_documents_path(project), params:)
+      login_as leader, scope: :user
+      post(project_documents_path(project), params:)
 
-        expect(response).to redirect_to project_documents_path(project)
-        expect(project.documents.count).to eq 1
-        expect(project.documents.last.title).to eq 'Teste de request'
-        expect(project.documents.last.description).to eq 'Descrição'
-        expect(project.documents.last.user).to eq leader
-        expect(project.documents.last.file.attached?).to be true
-        expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_jpg.jpg')
-      end
-
-      it 'por um colaborador' do
-        project = create(:project)
-        contributor = create(:user, cpf: '747.860.990-23', email: 'colaborador@email.com')
-        project.user_roles.create(user: contributor)
-        params = { document: {
-          title: 'Documento de contribuidor',
-          description: 'Descrição',
-          file: fixture_file_upload('spec/support/files/sample_png.png')
-        } }
-
-        login_as contributor, scope: :user
-        post(project_documents_path(project), params:)
-        project.reload
-
-        expect(response).to redirect_to project_documents_path(project)
-        expect(project.documents.last.user).to eq contributor
-        expect(project.documents.last.file.attached?).to be true
-        expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_png.png')
-      end
-
-      it 'com extensão mp4' do
-        project = create(:project)
-        contributor = create(:user, cpf: '616.782.230-18', email: 'contributor@email.com')
-        project.user_roles.create!(user: contributor)
-        params = { document: {
-          title: 'Arquivo mp4',
-          file: fixture_file_upload('spec/support/files/sample_video.mp4')
-        } }
-
-        login_as contributor, scope: :user
-        post(project_documents_path(project), params:)
-
-        expect(response).to redirect_to project_documents_path(project)
-        expect(project.documents.last.user).to eq contributor
-        expect(project.documents.last.file.attached?).to be true
-        expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_video.mp4')
-      end
-
-      it 'com extensão mp3' do
-        project = create(:project)
-        contributor = create(:user, cpf: '038.580.790-22', email: 'contributor@email.com')
-        project.user_roles.create!(user: contributor)
-        params = { document: {
-          title: 'Arquivo mp3',
-          file: fixture_file_upload('spec/support/files/sample_audio.mp3')
-        } }
-
-        login_as contributor, sope: :user
-        post(project_documents_path(project), params:)
-
-        expect(response).to redirect_to project_documents_path(project)
-        expect(project.documents.last.user).to eq contributor
-        expect(project.documents.last.file.attached?).to be true
-        expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_audio.mp3')
-      end
-
-      it 'com extensão docx' do
-        project = create(:project)
-        contributor = create(:user, cpf: '706.259.640-04', email: 'contributor@email.com')
-        project.user_roles.create!(user: contributor)
-        params = { document: {
-          title: 'Arquivo Word',
-          file: fixture_file_upload('spec/support/files/sample_word_file.docx')
-        } }
-
-        login_as contributor, scope: :user
-        post(project_documents_path(project), params:)
-
-        expect(response).to redirect_to project_documents_path(project)
-        expect(project.documents.last.user).to eq contributor
-        expect(project.documents.last.file.attached?).to be true
-        expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_word_file.docx')
-      end
-
-      it 'com extensão pdf' do
-        project = create(:project)
-        contributor = create(:user, cpf: '279.907.860-52', email: 'contributor@email.com')
-        project.user_roles.create!(user: contributor)
-        params = { document: {
-          title: 'Arquivo pdf',
-          file: fixture_file_upload('spec/support/files/sample_pdf.pdf')
-        } }
-
-        login_as contributor, scope: :user
-        post(project_documents_path(project), params:)
-
-        expect(response).to redirect_to project_documents_path(project)
-        expect(project.documents.last.user).to eq contributor
-        expect(project.documents.last.file.attached?).to be true
-        expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_pdf.pdf')
-      end
-
-      it 'com extensão xlsx' do
-        project = create(:project)
-        contributor = create(:user, cpf: '471.301.980-10', email: 'contributor@email.com')
-        project.user_roles.create!(user: contributor)
-        params = { document: {
-          title: 'Arquivo Excel',
-          file: fixture_file_upload('spec/support/files/sample_excel.xlsx')
-        } }
-
-        login_as contributor, scope: :user
-        post(project_documents_path(project), params:)
-
-        expect(response).to redirect_to project_documents_path(project)
-        expect(project.documents.last.user).to eq contributor
-        expect(project.documents.last.file.attached?).to be true
-        expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_excel.xlsx')
-      end
-
-      it 'com extensão ppt' do
-        project = create(:project)
-        contributor = create(:user, cpf: '939.351.790-81', email: 'contributor@email.com')
-        project.user_roles.create!(user: contributor)
-        params = { document: {
-          title: 'Arquivo Power Point',
-          file: fixture_file_upload('spec/support/files/sample_ppt.ppt')
-        } }
-
-        login_as contributor, scope: :user
-        post(project_documents_path(project), params:)
-
-        expect(response).to redirect_to project_documents_path(project)
-        expect(project.documents.last.user).to eq contributor
-        expect(project.documents.last.file.attached?).to be true
-        expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_ppt.ppt')
-      end
-
-      it 'com extensão csv' do
-        project = create(:project)
-        contributor = create(:user, cpf: '939.351.790-81', email: 'contributor@email.com')
-        project.user_roles.create!(user: contributor)
-        params = { document: {
-          title: 'Arquivo CSV',
-          file: fixture_file_upload('spec/support/files/sample_csv.csv')
-        } }
-
-        login_as contributor, scope: :user
-        post(project_documents_path(project), params:)
-
-        expect(response).to redirect_to project_documents_path(project)
-        expect(project.documents.last.user).to eq contributor
-        expect(project.documents.last.file.attached?).to be true
-        expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_csv.csv')
-      end
-
-      it 'com extensão zip' do
-        project = create(:project)
-        contributor = create(:user, cpf: '939.351.790-81', email: 'contributor@email.com')
-        project.user_roles.create!(user: contributor)
-        params = { document: {
-          title: 'Arquivo ZIP',
-          file: fixture_file_upload('spec/support/files/sample_zip.zip')
-        } }
-
-        login_as contributor, scope: :user
-        post(project_documents_path(project), params:)
-
-        expect(response).to redirect_to project_documents_path(project)
-        expect(project.documents.last.user).to eq contributor
-        expect(project.documents.last.file.attached?).to be true
-        expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_zip.zip')
-      end
+      expect(response).to redirect_to project_documents_path(project)
+      expect(project.documents.count).to eq 1
+      expect(project.documents.last.title).to eq 'Teste de request'
+      expect(project.documents.last.description).to eq 'Descrição'
+      expect(project.documents.last.user).to eq leader
+      expect(project.documents.last.file.attached?).to be true
+      expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_jpg.jpg')
     end
 
-    context 'sem sucesso' do
-      it 'por um usuário deslogado'
-      it 'por um usuário não pertencente ao projeto'
-      it 'com extensão não permitida'
+    it 'por um colaborador' do
+      project = create(:project)
+      contributor = create(:user, cpf: '747.860.990-23', email: 'colaborador@email.com')
+      project.user_roles.create(user: contributor)
+      params = { document: {
+        title: 'Documento de contribuidor',
+        description: 'Descrição',
+        file: fixture_file_upload('spec/support/files/sample_png.png')
+      } }
+
+      login_as contributor, scope: :user
+      post(project_documents_path(project), params:)
+      project.reload
+
+      expect(response).to redirect_to project_documents_path(project)
+      expect(project.documents.last.user).to eq contributor
+      expect(project.documents.last.file.attached?).to be true
+      expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_png.png')
+    end
+
+    it 'com extensão mp4' do
+      project = create(:project)
+      contributor = create(:user, cpf: '616.782.230-18', email: 'contributor@email.com')
+      project.user_roles.create!(user: contributor)
+      params = { document: {
+        title: 'Arquivo mp4',
+        file: fixture_file_upload('spec/support/files/sample_video.mp4')
+      } }
+
+      login_as contributor, scope: :user
+      post(project_documents_path(project), params:)
+
+      expect(response).to redirect_to project_documents_path(project)
+      expect(project.documents.last.user).to eq contributor
+      expect(project.documents.last.file.attached?).to be true
+      expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_video.mp4')
+    end
+
+    it 'com extensão mp3' do
+      project = create(:project)
+      contributor = create(:user, cpf: '038.580.790-22', email: 'contributor@email.com')
+      project.user_roles.create!(user: contributor)
+      params = { document: {
+        title: 'Arquivo mp3',
+        file: fixture_file_upload('spec/support/files/sample_audio.mp3')
+      } }
+
+      login_as contributor, sope: :user
+      post(project_documents_path(project), params:)
+
+      expect(response).to redirect_to project_documents_path(project)
+      expect(project.documents.last.user).to eq contributor
+      expect(project.documents.last.file.attached?).to be true
+      expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_audio.mp3')
+    end
+
+    it 'com extensão docx' do
+      project = create(:project)
+      contributor = create(:user, cpf: '706.259.640-04', email: 'contributor@email.com')
+      project.user_roles.create!(user: contributor)
+      params = { document: {
+        title: 'Arquivo Word',
+        file: fixture_file_upload('spec/support/files/sample_word_file.docx')
+      } }
+
+      login_as contributor, scope: :user
+      post(project_documents_path(project), params:)
+
+      expect(response).to redirect_to project_documents_path(project)
+      expect(project.documents.last.user).to eq contributor
+      expect(project.documents.last.file.attached?).to be true
+      expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_word_file.docx')
+    end
+
+    it 'com extensão pdf' do
+      project = create(:project)
+      contributor = create(:user, cpf: '279.907.860-52', email: 'contributor@email.com')
+      project.user_roles.create!(user: contributor)
+      params = { document: {
+        title: 'Arquivo pdf',
+        file: fixture_file_upload('spec/support/files/sample_pdf.pdf')
+      } }
+
+      login_as contributor, scope: :user
+      post(project_documents_path(project), params:)
+
+      expect(response).to redirect_to project_documents_path(project)
+      expect(project.documents.last.user).to eq contributor
+      expect(project.documents.last.file.attached?).to be true
+      expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_pdf.pdf')
+    end
+
+    it 'com extensão xlsx' do
+      project = create(:project)
+      contributor = create(:user, cpf: '471.301.980-10', email: 'contributor@email.com')
+      project.user_roles.create!(user: contributor)
+      params = { document: {
+        title: 'Arquivo Excel',
+        file: fixture_file_upload('spec/support/files/sample_excel.xlsx')
+      } }
+
+      login_as contributor, scope: :user
+      post(project_documents_path(project), params:)
+
+      expect(response).to redirect_to project_documents_path(project)
+      expect(project.documents.last.user).to eq contributor
+      expect(project.documents.last.file.attached?).to be true
+      expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_excel.xlsx')
+    end
+
+    it 'com extensão ppt' do
+      project = create(:project)
+      contributor = create(:user, cpf: '939.351.790-81', email: 'contributor@email.com')
+      project.user_roles.create!(user: contributor)
+      params = { document: {
+        title: 'Arquivo Power Point',
+        file: fixture_file_upload('spec/support/files/sample_ppt.ppt')
+      } }
+
+      login_as contributor, scope: :user
+      post(project_documents_path(project), params:)
+
+      expect(response).to redirect_to project_documents_path(project)
+      expect(project.documents.last.user).to eq contributor
+      expect(project.documents.last.file.attached?).to be true
+      expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_ppt.ppt')
+    end
+
+    it 'com extensão csv' do
+      project = create(:project)
+      contributor = create(:user, cpf: '939.351.790-81', email: 'contributor@email.com')
+      project.user_roles.create!(user: contributor)
+      params = { document: {
+        title: 'Arquivo CSV',
+        file: fixture_file_upload('spec/support/files/sample_csv.csv')
+      } }
+
+      login_as contributor, scope: :user
+      post(project_documents_path(project), params:)
+
+      expect(response).to redirect_to project_documents_path(project)
+      expect(project.documents.last.user).to eq contributor
+      expect(project.documents.last.file.attached?).to be true
+      expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_csv.csv')
+    end
+
+    it 'com extensão zip' do
+      project = create(:project)
+      contributor = create(:user, cpf: '939.351.790-81', email: 'contributor@email.com')
+      project.user_roles.create!(user: contributor)
+      params = { document: {
+        title: 'Arquivo ZIP',
+        file: fixture_file_upload('spec/support/files/sample_zip.zip')
+      } }
+
+      login_as contributor, scope: :user
+      post(project_documents_path(project), params:)
+
+      expect(response).to redirect_to project_documents_path(project)
+      expect(project.documents.last.user).to eq contributor
+      expect(project.documents.last.file.attached?).to be true
+      expect(project.documents.last.file.byte_size).to eq File.size('spec/support/files/sample_zip.zip')
+    end
+  end
+
+  context 'sem sucesso' do
+    it 'por um usuário deslogado' do
+      project = create(:project)
+      params = { document: {
+        title: 'Upload sem sucesso',
+        file: fixture_file_upload('spec/support/files/sample_csv.csv')
+      } }
+
+      post(project_documents_path(project), params:)
+
+      expect(response).to redirect_to new_user_session_path
+      expect(project.documents).to be_empty
+    end
+
+    it 'por um usuário não pertencente ao projeto'
+
+    it 'com extensão não permitida' do
+      project = create(:project)
+      contributor = create(:user, cpf: '866.726.620-57', email: 'contributor@email.com')
+      project.user_roles.create!(user: contributor)
+      params = { document: {
+        title: 'Documento com arquivo não suportado',
+        file: fixture_file_upload('spec/support/files/invalid_upload_file.txt')
+      } }
+
+      login_as contributor, scope: :user
+      post(project_documents_path(project), params:)
+
+      expect(response).to have_http_status :unprocessable_entity
+      expect(flash[:alert]).to eq 'Não foi possível adicionar o documento.'
+      expect(project.documents).to be_empty
     end
   end
 end
