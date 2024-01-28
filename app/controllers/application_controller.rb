8158dfb675e5dbafc6cb140e8fec6ac85f7cf836
Frederico <frederico.smozzato@gmail.com>
2024-01-28 13:54:31 -0300

    Redireciona o usuário para página de projetos ao fazer login
    Co-authored-bty: Lucas Vasques <lucasvc1996@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
   def not_found
     redirect_to root_path, alert: t('.not_found')
   end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || projects_path
+  end
 end
