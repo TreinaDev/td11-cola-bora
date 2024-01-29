963c2993c22a484af2cedb2dd46e392075dc87c6
Frederico <frederico.smozzato@gmail.com>
2024-01-29 14:17:37 -0300

    Adiciona seeds de reunião e projetos e corrige detalhes de layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,10 @@ pokemon_project = FactoryBot.create(:project, user: brock, title: 'Líder de Gin
                                     description: 'Me tornar líder do estádio de pedra.',
                                     category: 'Auto Ajuda')
 
+FactoryBot.create(:project, user: brock, title: 'Pokedex',
+                            description: 'Fazer uma listagem de todos os pokemons.',
+                            category: 'Tecnologia')
+
 FactoryBot.create(:task, project: pokemon_project, title:'Pegar um geodude',
                          description:'Para completar o meu time de pedra, preciso de um geodude, vamos captura-lo',
                          assigned: brock, due_date: 2.months.from_now.to_date, author: brock)
@@ -32,4 +36,39 @@ FactoryBot.create(:task, project: pokemon_project, title:'Derrotar um Charmander
                          assigned: ash, due_date: 1.week.from_now, author: ash)
 
 FactoryBot.create(:invitation, project: pokemon_project, profile_id: 1,
-                               expiration_days: 10, status: :pending)
+                            expiration_days: 10, status: :pending)
+
+FactoryBot.create(:meeting, project: pokemon_project, user_role: UserRole.find_by(user: brock, project: pokemon_project),
+                            title:'Reunião para planejar a captura do geodude',
+                            description:'Para completar o meu time de pedra, preciso de um geodude, vamos captura-lo',
+                            datetime: 2.days.from_now, duration: 75, address: 'https://meet.google.com/')
+
+FactoryBot.create(:meeting, project: pokemon_project, user_role: UserRole.find_by(user: brock, project: pokemon_project),
+                            title:'Parar a equipe rocket',
+                            description:'A equipe rocket está aprontando novamente, temos que para-los',
+                            datetime: (2.days.from_now + 2.hours), duration: 50, address: 'https://meet.google.com/')
+
+FactoryBot.create(:meeting, project: pokemon_project, user_role: UserRole.find_by(user: brock, project: pokemon_project),
+                            title:'Derrotar um Charmander',
+                            description:'Lutar contra outro treinador com um Charmander.',
+                            datetime: 5.days.from_now, duration: 120, address: 'https://meet.google.com/')
+
+FactoryBot.create(:meeting, project: pokemon_project, user_role: UserRole.find_by(user: brock, project: pokemon_project),
+                            title:'Daily', description:'', datetime: 1.days.from_now, duration: 15,
+                            address: 'https://meet.google.com/')
+
+FactoryBot.create(:meeting, project: pokemon_project, user_role: UserRole.find_by(user: brock, project: pokemon_project),
+                            title:'Daily', description:'', datetime: 2.days.from_now, duration: 15,
+                            address: 'https://meet.google.com/')
+
+FactoryBot.create(:meeting, project: pokemon_project, user_role: UserRole.find_by(user: brock, project: pokemon_project),
+                            title:'Daily', description:'', datetime: 3.days.from_now, duration: 15,
+                            address: 'https://meet.google.com/')
+
+FactoryBot.create(:meeting, project: pokemon_project, user_role: UserRole.find_by(user: brock, project: pokemon_project),
+                            title:'Daily', description:'', datetime: 4.days.from_now, duration: 15,
+                            address: 'https://meet.google.com/')
+
+FactoryBot.create(:meeting, project: pokemon_project, user_role: UserRole.find_by(user: brock, project: pokemon_project),
+                            title:'Daily', description:'', datetime: 5.days.from_now, duration: 15,
+                            address: 'https://meet.google.com/')
