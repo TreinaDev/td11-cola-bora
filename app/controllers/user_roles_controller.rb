dd9202639a037c27a0f16add1fde183efe4b6f51
Frederico <frederico.smozzato@gmail.com>
2024-02-05 09:19:01 -0300

    Refatora user_roles_controller
    Co-authored-by: Paulo Henrique Meneses <ph.1928@hotmail.com>
    Co-authored-by: Mateus Cavedini <mateuscvdn@gmail.com>

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -1,6 +1,6 @@
 class UserRolesController < ApplicationController
-  before_action :set_project, only: %i[edit update]
   before_action :set_user_role, only: %i[edit update remove]
+  before_action :set_project, only: %i[edit update remove]
   before_action :redirect_if_role_is_leader, only: %i[edit update remove]
   before_action :authorize_leader, only: %i[edit update]
 
@@ -16,18 +16,16 @@ class UserRolesController < ApplicationController
   end
 
   def remove
-    if @user_role.project.leader? current_user
-      @user_role.update active: false
-      redirect_to members_project_path(@user_role.project), notice: t('.success')
-    else
-      redirect_to root_path, alert: t('.fail')
-    end
+    return redirect_to root_path, alert: t('.fail') unless @project.leader? current_user
+
+    @user_role.update active: false
+    redirect_to members_project_path(@project), notice: t('.success')
   end
 
   private
 
   def set_project
-    @project = Project.find(params[:project_id])
+    @project = Project.find_by(id: params[:project_id]) || @user_role.project
   end
 
   def set_user_role
