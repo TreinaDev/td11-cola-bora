e2980f170a7666ecbef7dfb7876fac171350360c
decoporteira <andreporteira@gmail.com>
2024-02-06 16:26:07 -0300

    Adiciona teste faltante em InvitationController
    Co-authored-by: Adoniran France <adoniranfranceh@users.noreply.github.com>

--- spec/requests/invitations/user_send_invitation_request_spec.rb
+++ spec/requests/invitations/user_send_invitation_request_spec.rb
@@ -22,4 +22,26 @@ describe 'Usuário envia um convite sendo lider' do
     expect(flash[:alert]).to eq 'Esse usuário possui convite pendente'
     expect(response).to redirect_to project_portfoliorrr_profile_path(project, joao.id)
   end
+
+  it 'e falha' do
+    user = create(:user)
+    project = create(:project, user:, title: 'Meu novo projeto')
+    create(:project, user:, title: 'Segundo projeto')
+    joao = PortfoliorrrProfile.new(id: 1, name: 'João Marcos',
+                                   job_categories: [JobCategory.new(id: 1, name: 'Desenvolvimento')])
+    joao.email = 'joao@email.com'
+    create :invitation, project:, profile_id: joao.id, status: :accepted, profile_email: joao.email
+
+    login_as user
+    params = {
+      invitation: {
+        profile_email: joao.email,
+        profile_id: joao.id
+      }
+    }
+    post(project_portfoliorrr_profile_invitations_path(project, joao.id), params:)
+
+    expect(flash[:alert]).to eq 'Não foi possível convidar o usuário'
+    expect(response).to redirect_to project_portfoliorrr_profile_path(project, joao.id)
+  end
 end
