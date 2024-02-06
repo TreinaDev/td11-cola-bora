45abb9ab025317e915a3cbde064a713a5e6b1f21
Moises Almeida <moisesssprg@gmail.com>
2024-02-06 17:12:37 -0300

    Adiciona testes de requisição
    Co-authored-by: Mateus Cavedini <mateuscavedini@users.noreply.github.com>
    Co-authored-by: Valéria Carneiro <valerialrc@users.noreply.github.com>

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,6 @@
 class CalendarsController < ApplicationController
   before_action :set_project, only: %i[index]
+  before_action :authorize_member, only: %i[index]
 
   def index
     filter = params[:filter]
@@ -12,5 +13,9 @@ class CalendarsController < ApplicationController
 
   def set_project
     @project = Project.find(params[:project_id])
+  end
+
+  def authorize_member
+    redirect_to root_path, alert: t(:unauthorized) unless @project.member? current_user
   end
 end
