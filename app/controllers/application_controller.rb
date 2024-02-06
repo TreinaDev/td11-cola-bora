5b05fc8eaab27f3fc036f9ce48d4c4f091a2ff89
decoporteira <andreporteira@gmail.com>
2024-02-06 15:50:51 -0300

    Remove rescue_from ActiveRecord::RecordNotFound de ApplicationController e remove seu respectivo teste.
    Co-authored-by: Adoniran France <adoniranfranceh@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
-
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:cpf])
-  end
-
-  def not_found
-    redirect_to root_path, alert: t('.not_found')
   end
 
   def after_sign_in_path_for(resource)
