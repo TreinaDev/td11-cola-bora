d2f8537bf9f67c361ac864b317934faf27359ded
decoporteira <andreporteira@gmail.com>
2024-02-07 18:01:25 -0300

    Adiciona traduções para a view do email.
    Co-authored-by: Adoniran France <adoniranfranceh@users.noreply.github.com>

--- spec/mailers/task_mailer_spec.rb
+++ spec/mailers/task_mailer_spec.rb
@@ -8,14 +8,15 @@ RSpec.describe TaskMailer, type: :mailer do
       create(:profile, first_name: 'Giovanni', last_name: '', user: contributor)
       contributor_role = create(:user_role, project:, user: contributor)
       task = create(:task, title: 'Captura 150 pokemons', user_role: contributor_role, project:)
-      url = project_task_path(project, task)
+      url = project_task_url(project, task, host: 'http://127.0.0.1:3000/')
 
       mail = TaskMailer.with(task:, url:).notify_leader_finish_task
 
       expect(mail.body.encoded).to include 'A tarefa Captura 150 pokemons do projeto Mestre Pokémon'
-      expect(mail.body.encoded).to include 'foi finalizada por Giovanni.<br>'
+      expect(mail.body.encoded).to include 'foi finalizada por Giovanni.'
       expect(mail.subject).to include 'A tarefa foi finalizada.'
-      expect(mail.body.encoded).to include "Para ver a tarefa pronta, acesse: #{project_task_path(project, task)}."
+      link = '<a href="http://127.0.0.1/projects/1/tasks/1">Para ver a tarefa pronta, acesse</a>'
+      expect(mail.body.encoded).to include link
     end
   end
 end
