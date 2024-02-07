90d2538cde32a992f62fcb8c99717e8b55f39d44
Moises Almeida <moisesssprg@gmail.com>
2024-02-07 15:41:47 -0300

    Configura solid_queue para desenvolvimento e refatora teste do job
    Co-authored-by: Mateus Cavedini <mateuscavedini@users.noreply.github.com>
    Co-authored-by: Valéria Carneiro <valerialrc@users.noreply.github.com>

--- spec/system/tasks/job_expires_task_spec.rb
+++ spec/system/tasks/job_expires_task_spec.rb
@@ -4,15 +4,24 @@ describe 'Job expira tarefa' do
   it 'em progresso' do
     author = create(:user, email: 'joão@email.com', password: 'password')
     project = create(:project, user: author)
-    task = create(:task, project:, due_date: 2.days.from_now.to_date, status: 'in_progress')
 
     expired_task_spy = spy(ExpiredTaskJob)
     stub_const('ExpiredTaskJob', expired_task_spy)
 
-    ExpiredTaskJob.set(wait_until: task.due_date.end_of_day).perform_later(task)
+    task = create(:task, project:, due_date: 2.days.from_now.to_date, status: 'in_progress')
 
     expect(expired_task_spy).to have_received(:perform_later).with(task).once
   end
 
-  xit 'não iniciado'
+  it 'não iniciado' do
+    author = create(:user, email: 'joão@email.com', password: 'password')
+    project = create(:project, user: author)
+
+    expired_task_spy = spy(ExpiredTaskJob)
+    stub_const('ExpiredTaskJob', expired_task_spy)
+
+    task = create(:task, project:, due_date: 2.days.from_now.to_date, status: 'uninitialized')
+
+    expect(expired_task_spy).to have_received(:perform_later).with(task).once
+  end
 end
