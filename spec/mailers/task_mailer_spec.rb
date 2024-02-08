0cca81f58eb10130882aab4b2453518522a4ac14
decoporteira <andreporteira@gmail.com>
2024-02-08 10:22:02 -0300

    Altera nome no email caso o leader seja quem finalizou a tarefa.
    Co-authored-by: Adoniran France <adoniranfranceh@users.noreply.github.com>

--- spec/mailers/task_mailer_spec.rb
+++ spec/mailers/task_mailer_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe TaskMailer, type: :mailer do
   describe '#notify_leader_finish_task' do
-    it 'e crio o email com sucesso' do
+    it 'e colaborador finaliza tarefa com sucesso' do
       project = create(:project, title: 'Mestre Pokémon')
+      project.user.profile.first_name = 'Ash Ketchum'
       contributor = create(:user, cpf: '167.882.240-05')
       create(:profile, first_name: 'Giovanni', last_name: '', user: contributor)
       contributor_role = create(:user_role, project:, user: contributor)
@@ -12,8 +13,27 @@ RSpec.describe TaskMailer, type: :mailer do
 
       mail = TaskMailer.with(task:, url:).notify_leader_finish_task
 
+      expect(mail.body.encoded).to include 'Olá Ash Ketchum. Uma tarefa foi finalizada!'
       expect(mail.body.encoded).to include 'A tarefa Captura 150 pokemons do projeto Mestre Pokémon'
       expect(mail.body.encoded).to include 'foi finalizada por Giovanni.'
+      expect(mail.subject).to include 'A tarefa foi finalizada.'
+      link = '<a href="http://127.0.0.1/projects/1/tasks/1">Para ver a tarefa pronta, acesse</a>'
+      expect(mail.body.encoded).to include link
+    end
+
+    it 'e lider finaliza tarefa com sucesso' do
+      project = create(:project, title: 'Mestre Pokémon')
+      create(:user, cpf: '167.882.240-05')
+      project.user.profile.first_name = 'Ash Ketchum'
+      author = project.user.user_roles.first
+      task = create(:task, title: 'Captura 150 pokemons', user_role: author, project:)
+      url = project_task_url(project, task, host: 'http://127.0.0.1:3000/')
+
+      mail = TaskMailer.with(task:, url:).notify_leader_finish_task
+
+      expect(mail.body.encoded).to include 'Olá Ash Ketchum. Uma tarefa foi finalizada!'
+      expect(mail.body.encoded).to include 'A tarefa Captura 150 pokemons do projeto Mestre Pokémon'
+      expect(mail.body.encoded).to include 'foi finalizada por você.'
       expect(mail.subject).to include 'A tarefa foi finalizada.'
       link = '<a href="http://127.0.0.1/projects/1/tasks/1">Para ver a tarefa pronta, acesse</a>'
       expect(mail.body.encoded).to include link
