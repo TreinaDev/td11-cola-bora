1c6b60b5aef2a6f7e18f64d94eceb9224bdf7324
Valéria Carneiro <valerialribeiroc@gmail.com>
2024-02-08 10:44:29 -0300

    Corrige testes de acordo com a factory
    Co-authored-by: Mateus Cavedini <mateuscvdn@gmail.com>
    Co-authored-by: Moises Almeida <moisesssprg@gmail.com>

--- spec/jobs/tasks/expire_task_job_spec.rb
+++ spec/jobs/tasks/expire_task_job_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe ExpireTaskJob, type: :job do
   describe '#perform' do
     context 'muda para expirado quando excede prazo' do
       it 'e não está iniciado' do
-        task = create :task, due_date: 5.days.from_now, status: :uninitialized
+        project = create :project
+        contributor = create :user, cpf: '979.612.040-24'
+        user_role = create :user_role, project:, user: contributor, role: :contributor
+        task = create :task, due_date: 5.days.from_now, user_role:, project:, status: :uninitialized
 
         travel_to 5.days.from_now do
           ExpireTaskJob.perform_now task
@@ -14,7 +17,10 @@ RSpec.describe ExpireTaskJob, type: :job do
       end
 
       it 'e está em andamento' do
-        task = create :task, due_date: 5.days.from_now, status: :in_progress
+        project = create :project
+        contributor = create :user, cpf: '979.612.040-24'
+        user_role = create :user_role, project:, user: contributor, role: :contributor
+        task = create :task, due_date: 5.days.from_now, user_role:, project:, status: :in_progress
 
         travel_to 5.days.from_now do
           ExpireTaskJob.perform_now task
@@ -24,7 +30,10 @@ RSpec.describe ExpireTaskJob, type: :job do
       end
 
       it 'e a tarefa foi criada com prazo e depois foi alterada para prazo maior' do
-        task = create(:task, due_date: 1.day.from_now.to_date, status: 'uninitialized')
+        project = create :project
+        contributor = create :user, cpf: '979.612.040-24'
+        user_role = create :user_role, project:, user: contributor, role: :contributor
+        task = create :task, due_date: 1.day.from_now, user_role:, project:, status: :uninitialized
 
         task.update(due_date: 2.days.from_now.to_date)
 
@@ -40,7 +49,10 @@ RSpec.describe ExpireTaskJob, type: :job do
       end
 
       it 'e a tarefa foi criada com prazo e depois foi alterada para prazo menor' do
-        task = create(:task, due_date: 2.days.from_now.to_date, status: 'uninitialized')
+        project = create :project
+        contributor = create :user, cpf: '979.612.040-24'
+        user_role = create :user_role, project:, user: contributor, role: :contributor
+        task = create :task, due_date: 2.days.from_now, user_role:, project:, status: :uninitialized
 
         task.update(due_date: 1.day.from_now.to_date)
 
@@ -53,7 +65,10 @@ RSpec.describe ExpireTaskJob, type: :job do
 
     context 'não muda para expirado' do
       it 'quando está cancelado' do
-        task = create :task, due_date: 5.days.from_now, status: :cancelled
+        project = create :project
+        contributor = create :user, cpf: '979.612.040-24'
+        user_role = create :user_role, project:, user: contributor, role: :contributor
+        task = create :task, due_date: 5.days.from_now, user_role:, project:, status: :cancelled
 
         travel_to 5.days.from_now do
           ExpireTaskJob.perform_now task
@@ -63,7 +78,10 @@ RSpec.describe ExpireTaskJob, type: :job do
       end
 
       it 'quando está finalizado' do
-        task = create :task, due_date: 5.days.from_now, status: :finished
+        project = create :project
+        contributor = create :user, cpf: '979.612.040-24'
+        user_role = create :user_role, project:, user: contributor, role: :contributor
+        task = create :task, due_date: 5.days.from_now, user_role:, project:, status: :finished
 
         travel_to 5.days.from_now do
           ExpireTaskJob.perform_now task
@@ -73,7 +91,10 @@ RSpec.describe ExpireTaskJob, type: :job do
       end
 
       it 'e a tarefa foi criada com prazo e depois foi alterada para sem prazo' do
-        task = create(:task, due_date: 1.day.from_now.to_date, status: 'uninitialized')
+        project = create :project
+        contributor = create :user, cpf: '979.612.040-24'
+        user_role = create :user_role, project:, user: contributor, role: :contributor
+        task = create :task, due_date: 5.days.from_now, user_role:, project:, status: :uninitialized
 
         task.update(due_date: nil)
 
--- spec/system/tasks/job_updates_task_status_spec.rb
+++ spec/system/tasks/job_updates_task_status_spec.rb
@@ -2,33 +2,36 @@ require 'rails_helper'
 
 describe 'Job expira tarefa' do
   it 'em progresso' do
-    author = create(:user, email: 'joão@email.com', password: 'password')
-    project = create(:project, user: author)
+    project = create :project
+    author = create :user, cpf: '979.612.040-24'
+    author_role = create :user_role, project:, user: author, role: :contributor
 
     expired_task_spy = spy(ExpireTaskJob)
     stub_const('ExpireTaskJob', expired_task_spy)
 
-    task = create(:task, project:, due_date: 2.days.from_now.to_date, status: 'in_progress')
+    task = create(:task, project:, user_role: author_role, due_date: 2.days.from_now.to_date, status: :in_progress)
 
     expect(expired_task_spy).to have_received(:perform_later).with(task).once
   end
 
   it 'não iniciado' do
-    author = create(:user, email: 'joão@email.com', password: 'password')
-    project = create(:project, user: author)
+    project = create :project
+    author = create :user, cpf: '979.612.040-24'
+    author_role = create :user_role, project:, user: author, role: :contributor
 
     expired_task_spy = spy(ExpireTaskJob)
     stub_const('ExpireTaskJob', expired_task_spy)
 
-    task = create(:task, project:, due_date: 2.days.from_now.to_date, status: 'uninitialized')
+    task = create(:task, project:, user_role: author_role, due_date: 2.days.from_now.to_date, status: :uninitialized)
 
     expect(expired_task_spy).to have_received(:perform_later).with(task).once
   end
 
   it 'quando uma tarefa é criada sem prazo e depois atualizada com prazo' do
-    author = create(:user, email: 'joão@email.com', password: 'password')
-    project = create(:project, user: author)
-    task = create(:task, project:, due_date: nil, status: 'uninitialized')
+    project = create :project
+    author = create :user, cpf: '979.612.040-24'
+    author_role = create :user_role, project:, user: author, role: :contributor
+    task = create(:task, project:, user_role: author_role, due_date: nil, status: :uninitialized)
 
     expired_task_spy = spy(ExpireTaskJob)
     stub_const('ExpireTaskJob', expired_task_spy)
@@ -39,12 +42,14 @@ describe 'Job expira tarefa' do
   end
 
   it 'quando a tarefa é criada com prazo e depois é alterada para prazo maior' do
-    author = create(:user, email: 'joão@email.com', password: 'password')
-    project = create(:project, user: author)
+    project = create :project
+    author = create :user, cpf: '979.612.040-24'
+    author_role = create :user_role, project:, user: author, role: :contributor
+
     expired_task_spy = spy(ExpireTaskJob)
     stub_const('ExpireTaskJob', expired_task_spy)
 
-    task = create(:task, project:, due_date: 1.day.from_now.to_date, status: 'uninitialized')
+    task = create(:task, project:, user_role: author_role, due_date: 1.day.from_now.to_date, status: 'uninitialized')
 
     task.update(due_date: 2.days.from_now.to_date)
 
