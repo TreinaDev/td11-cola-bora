3c3a511bc276258d0d4f8358e5c99be6de9387e6
Moises Almeida <moisesssprg@gmail.com>
2024-02-08 12:01:16 -0300

    WIP: adiciona teste de sistema
    Co-authored-by: Valéria Carneiro <valerialrc@users.noreply.github.com>
    Co-authored-by: Mateus Cavedini <mateuscavedini@users.noreply.github.com>

---
+++ spec/system/meetings/meeting_participants/user_adds_meeting_participants_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'Usuário adiciona participantes a reunião' do
+  xit 'com sucesso'
+  xit 'e não vê usuários que não fazem parte do projeto'
+  xit 'e ninguém é adicionado'
+end
