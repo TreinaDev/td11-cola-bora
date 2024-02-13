a4f88c9950bd48ffab2588703cc24dc5ef41468d
Adoniran <adoniranfranca@gmail.com>
2024-02-13 13:01:10 -0300

    Adiciona pesquisa por título e corpo de postagem
    Co-authored-by: André Pereira <andreporteira@gmail.com>

--- app/javascript/components/forum_vue.js
+++ app/javascript/components/forum_vue.js
@@ -3,15 +3,33 @@ import { ref } from "vue/dist/vue.esm-bundler.js"
 export default {
   data() {
     return {
-      message: 'Hello!',
-      insertText: '',
+      searchText: '',
+      selectedFilter: '',
       project: window.project,
       posts: []
     }
   },
+
   mounted() {
     this.posts = window.posts.map(item => ({ title: item.title, body: item.body }));
   },
+
+  computed:{
+    filteredProjects() {
+      const searchType = this.selectedFilter
+      return this.posts.filter(post => {
+        if (searchType === '') {
+          return (
+            post.title.toLowerCase().includes(this.searchText.toLowerCase()) ||
+            post.body.toLowerCase().includes(this.searchText.toLowerCase())
+          )
+        } else {
+          return post[searchType].toLowerCase().includes(this.searchText.toLowerCase());
+        }
+      })
+    }
+  },
+
   methods: {
     insertMessage() {
       this.message = this.insertText;
