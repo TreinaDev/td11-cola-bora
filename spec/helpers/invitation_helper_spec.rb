df47d033a0e93b47a00a0fff03aadd50561bfa44
Frederico <frederico.smozzato@gmail.com>
2024-02-13 17:56:44 -0300

    Remove testes com tempo
    Co-authored-by: Lucas GO Aguilar <olucasaguilar@users.noreply.github.com>

--- spec/helpers/invitation_helper_spec.rb
+++ spec/helpers/invitation_helper_spec.rb
@@ -13,21 +13,5 @@ RSpec.describe InvitationHelper, type: :helper do
 
       expect(invitation_expiration_date(invitation)).to eq 'Expira em 4 dias'
     end
-
-    it 'retorna tempo em horas para expiração do convite' do
-      travel_to Time.zone.today.noon do
-        invitation = create :invitation, expiration_days: 1
-
-        expect(invitation_expiration_date(invitation)).to eq 'Expira em aproximadamente 12 horas'
-      end
-    end
-
-    it 'retorna tempo em minutos para expiração do convite' do
-      travel_to Time.zone.today.beginning_of_day do
-        invitation = create :invitation, expiration_days: 0
-
-        expect(invitation_expiration_date(invitation)).to eq 'Expira em menos de um minuto'
-      end
-    end
   end
 end
