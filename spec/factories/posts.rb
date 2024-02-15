95187b58ecf129711549b09e3861c249c0d124d0
decoporteira <andreporteira@gmail.com>
2024-02-15 17:26:03 -0300

    Alterações na factory Post.
    Co-authored-by: Adoniran France <adoniranfranceh@users.noreply.github.com>

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :post do
     title { 'A arte de criar' }
     body { 'Estive desenvolvendo um novo método para acelerar a resolução de tarefas do o projeto' }
-    user_role { nil }
-    project { nil }
+    user_role
+    project
   end
 end
